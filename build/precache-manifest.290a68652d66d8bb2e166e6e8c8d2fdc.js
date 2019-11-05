695be7602c347bc1723cadb21a3abd13e0cfad45
Your Name <you@example.com>
2019-11-05 18:58:46 +0800

    提交项目

---
+++ build/precache-manifest.290a68652d66d8bb2e166e6e8c8d2fdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2bb00ec3535f8ede37f5cd64c54431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2456c16ee56dd7949c34",
+    "url": "/static/css/2.dae7720b.chunk.css"
+  },
+  {
+    "revision": "5a477e9d12da9109fb91",
+    "url": "/static/css/main.652b21eb.chunk.css"
+  },
+  {
+    "revision": "2456c16ee56dd7949c34",
+    "url": "/static/js/2.67936494.chunk.js"
+  },
+  {
+    "revision": "5a477e9d12da9109fb91",
+    "url": "/static/js/main.790cf9dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
